4ab022fe854c7f61908fdeb7db57e5184b0782ff
Cristiano Soares <criscoder@msn.com>
2021-04-14 08:17:45 -0300

    implementa pesquisa básica de quadrinhos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { loadComics } from './utils/load-comics';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    comics: [],
+    isLoading: false
+  }
+  
+  componentDidMount() {
+    this.loadComics();
+  }
+
+  loadComics = async query => {
+    this.setState({
+      isLoading: true
+    });
+    await loadComics(query)
+      .then(res => {
+        const comics = res.data.data.results;
+        this.setState({
+          comics,
+          isLoading: false
+        });
+      })
+  }
+
+  handleChange = e => {
+    const query = e.target.value;
+    this.loadComics(query);
+  }
+
+  render() {
+    const { comics, isLoading } = this.state;
+    return (
+      <div>
+        <div className="search-wrapper">
+          <input type="search" className="search-box" placeholder="Pesquisar quadrinho..." onChange={this.handleChange} />
+        </div>
+        {
+          isLoading && (
+            <h5 className="mt-5">Carregando...</h5>
+          )
+        }
+        {
+          !isLoading && comics.length === 0 && (
+            <h5 className="mt-5">Nenhum quadrinho encontrado</h5>
+          )
+        }
+        {
+          !isLoading && comics.length > 0 && (
+            <ul className="comics-wrapper">
+              {comics.map(comic => <li key={comic.id} className="comic">
+                <img src={comic.thumbnail.path + '.' + comic.thumbnail.extension} alt={comic.title} />
+                <a className="comic-title">{comic.title}</a>
+              </li>)}
+            </ul>
+          )
+        }
+
+      </div>
+
+    )
+  }
 }
-
-export default App;
