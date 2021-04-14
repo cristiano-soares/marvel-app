3f0e6e318180c8557330589b77e146aa3569bece
Cristiano Soares <criscoder@msn.com>
2021-04-14 09:15:16 -0300

    cria componente Loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { loadComics } from './utils/load-comics';
+import { Loader } from './components/Loader';
 import './App.scss';
 
 export default class App extends React.Component {
   state = {
     comics: [],
+    selectedComics: new Set(),
     isLoading: false
   }
-  
+
   componentDidMount() {
     this.loadComics();
   }
@@ -26,21 +28,44 @@ export default class App extends React.Component {
       })
   }
 
+  handleSelection = comic => {
+    const { selectedComics } = this.state;
+    if (selectedComics.has(comic)) {
+      selectedComics.delete(comic);
+      comic.isSelected = false;
+    } else {
+      selectedComics.add(comic);
+      comic.isSelected = true;
+    }
+    this.setState({
+      selectedComics
+    })
+  }
+
+  handleSelectionCancel = () => {
+    const { selectedComics } = this.state;
+    selectedComics.forEach(comic => comic.isSelected = false);
+    this.setState({
+      selectedComics: new Set()
+    })
+  }
+
   handleChange = e => {
     const query = e.target.value;
     this.loadComics(query);
   }
 
   render() {
-    const { comics, isLoading } = this.state;
+    const { comics, isLoading, selectedComics } = this.state;
     return (
       <div>
+        
         <div className="search-wrapper">
           <input type="search" className="search-box" placeholder="Pesquisar quadrinho..." onChange={this.handleChange} />
         </div>
         {
           isLoading && (
-            <h5 className="mt-5">Carregando...</h5>
+            <Loader></Loader>
           )
         }
         {
@@ -51,11 +76,25 @@ export default class App extends React.Component {
         {
           !isLoading && comics.length > 0 && (
             <ul className="comics-wrapper">
-              {comics.map(comic => <li key={comic.id} className="comic">
-                <img src={comic.thumbnail.path + '.' + comic.thumbnail.extension} alt={comic.title} />
-                <a className="comic-title">{comic.title}</a>
-              </li>)}
+              {
+                comics.map(
+                  comic =>
+                    <li key={comic.id} className={"comic" + (comic.isSelected ? ' selected' : '')} onClick={() => this.handleSelection(comic)}>
+                      <img src={comic.thumbnail.path + '.' + comic.thumbnail.extension} alt={comic.title} />
+                      <a className="comic-title">{comic.title}</a>
+                    </li>
+                )}
             </ul>
+          )
+        }
+
+        {
+          selectedComics.size > 0 && (
+            <div className="footer">
+              <span>{selectedComics.size + (selectedComics.size > 1 ? ' quadrinhos selecionados' : ' quadrinho selecionado')}</span>
+              <button type="button">Enviar por e-mail</button>
+              <button type="button" onClick={this.handleSelectionCancel}>Cancelar</button>
+            </div>
           )
         }
 
---
+++ src/components/Loader/index.jsx
@@ -0,0 +1,7 @@
+import './styles.scss';
+
+export const Loader = () => (
+    <div class="spinner-grow-backdrop">
+        <div class="spinner-grow"></div>
+    </div>
+)
